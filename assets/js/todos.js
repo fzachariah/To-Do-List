f2c8afb0cf1d966adfe93f1d15e2f10605bfc8e4
FEBIN ZACHARIAH <febinzachariah6@gmail.com>
2017-06-12 17:10:13 -0400

    Final Commit

---
+++ assets/js/todos.js
@@ -0,0 +1,30 @@
+// Todo Click Check Off
+
+$("ul").on("click","li",function(){
+
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click","span",function(event){
+
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    })
+    event.stopPropagation();
+
+});
+
+$("input[type='text']").keypress(function(event){
+
+    if(event.which===13)
+    {
+        var todoText=$(this).val();
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> "+todoText+"</li>");
+        $(this).val("");
+    }
+    
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
